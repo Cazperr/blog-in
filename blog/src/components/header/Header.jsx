b1f9f97adde59b338a0ecdb75430bd9f64c466b8
soujabbar <solaimanpro10@gmail.com>
2022-06-20 00:35:03 +0200

    agregando todos los ficheros

---
+++ blog/src/components/header/Header.jsx
@@ -0,0 +1,42 @@
+import "./header.css";
+import Carousel from 'nuka-carousel';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+const Header = () => {
+  const PF = "http://localhost:7000/images/";
+
+  const [posts,setPosts] = useState([]);
+	useEffect(()=>{
+		const fetchPost = async () =>{
+		  const res = await axios.get("/posts")
+		  setPosts(res.data);
+		}
+		fetchPost();
+	  },[])
+
+    let post = posts.map((post) => post)
+     // console.log(post)
+
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleLg"> Blog<span className="corche">[</span><span className="inBlog">in</span><span className="corche">].</span> </span>
+        <span className="headerTitleSm">Express Yourself</span>
+      </div>
+      <Carousel wrapAround={true} autoplay >
+      {post.map((item) =>  (
+          <Link to={`/post/${item._id}`} className="link"> {/*Redirige a la página del post con su id */}
+            <img className="headerImg" src={PF + item.photo} />
+          </Link>
+        ))}
+      </Carousel>
+
+    </div>
+  );
+};
+
+export default Header;
---
+++ blog/src/pages/dashboard/LineChart.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Line } from "react-chartjs-2";
+import {Chart as ChartJS} from "chart.js/auto"
+import axios from 'axios';
+
+
+const LineChart = () => {
+
+	const [visits, setVisits] = useState([]);
+
+	/*const hexa = () =>{
+		let colorHex = (Math.random() * 0xffffff * 1000000).toString(16);
+		return `#${colorHex.slice(0, 6)}`;
+	}*/
+
+	useEffect(() => {
+		const fetchVisits = async () => {
+			const res = await axios.get("/visitas")
+			setVisits(res.data);
+		}
+		fetchVisits();
+	},[])
+
+	const visitasAño = visits.map((visitas) => visitas.totalAño);
+	const visitasDia = visits.map((visitas) => visitas.totalDia);
+	const visitasMes = visits.map((visitas) => visitas.totalMes);
+	const visitasTotal = visitasDia[0]+visitasMes[0]+visitasAño[0];
+
+	const dataVisit = [
+		{
+			"name": "Visitas Dia",
+			"visitas":  visitasDia[0],
+		},
+		{
+			"name": "Visitas Mes",
+			"visitas": visitasMes[0],
+		},
+		{
+			"name": "Visitas Año",
+			"visitas": visitasAño[0],
+		},
+		{
+			"name": "Visitas Año",
+			"visitas": visitasTotal,
+		},
+	];
+
+	const vis = {
+		labels: dataVisit.map((data) => data.name),
+		datasets: [{
+			label: "VISITAS",
+			data: dataVisit.map((data) => data.visitas),
+			backgroundColor: ["#f8bb51", "#c61d94", "#c6bb1d"],
+		}],
+	}
+
+	return (
+		<Line data={vis}/>
+	)
+};
+
+export default LineChart
