ba544e0f9cbfa95577cf25e8d292fb352a40cc2a
soujabbar <solaimanpro10@gmail.com>
2022-06-20 01:19:32 +0200

    cambios varios css

--- blog/src/pages/dashboard/Dashboard.jsx
+++ blog/src/pages/dashboard/Dashboard.jsx
@@ -1,30 +1,31 @@
-import "./dashboard.scss"
+import "./dashboard.scss";
 import BarChart from "./BarChart";
 import SidebarDash from "../../components/dashSidebar/SidebarDash";
 import LineChart from "./LineChart";
 import ChartUsers from "./ChartUsers";
 
-
-
 const Dashboard = () => {
-
-	return (
-		<div className='dashboardHome'>
-			<SidebarDash />
-			<div className="homeContainer">
-				<div className="centerItems">
-					<div className="charts">
-						<LineChart />
-					</div>
-					<div className="charts">
-						<BarChart />
-					</div>
-					<div className="charts">
-						<ChartUsers />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
-export default Dashboard
+  return (
+    <div className="dashboardHome">
+      <SidebarDash />
+      <div className="homeContainer">
+        <div className="centerItems">
+          <div className="left">
+            <div className="charts">
+              <LineChart />
+            </div>
+            <div className="charts">
+              <BarChart />
+            </div>
+          </div>
+          <div className="right">
+            <div className="circleBig">
+              <ChartUsers />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
